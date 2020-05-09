ec23c55108a0011bc88becadda8a2bf65e8dd9e8
Doc813 <tyto.alba.1999@gmail.com>
2020-05-09 23:06:47 +0300

    Hot Fix v.1.1.1

--- src/test/CalculatorTests.java
+++ src/test/CalculatorTests.java
@@ -6,11 +6,11 @@ public class CalculatorTests {
     @Test
     public void testCalculate() {
         Calculator calc = new Calculator();
-        int num1, num2;
+        double num1, num2;
         double res;
         for (int i = 0; i < 100; i++) {
-            num1 = (int) ( Math.random() * 1000 + 1 );
-            num2 = (int) ( Math.random() * 1000 + 1 );
+            num1 = (double) ( Math.random() * 1000 + 1 );
+            num2 = (double) ( Math.random() * 1000 + 1 );
             calc.Set_First_op(num1);
             calc.Set_Second_op(num2);
             calc.Set_operation('+');
@@ -24,6 +24,9 @@ public class CalculatorTests {
             assertEquals(res, calc.Calculate(), 0.00001);
             calc.Set_operation('*');
             res = num1*num2;
+            assertEquals(res, calc.Calculate(), 0.00001);
+            calc.Set_operation('^');
+            res = Math.pow(num1, num2);
             assertEquals(res, calc.Calculate(), 0.00001);
         }  
     }
